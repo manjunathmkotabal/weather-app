a63c2b0fdac454f5950af5ac658070f4e89e4654
matu <salinasmateo@oulook.com>
2023-03-12 18:24:51 -0300

    eslint rules

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
 
-const Weather = ({weather}) => {
+const Weather = ({ weather }) => {
   return (
     <div className='relative flex flex-col justify-between max-w-[500px] h-[70vh] m-auto p-4 text-gray-300 z-10 '>
         <div className='relative flex justify-center pt-10 items-center'>
             <div className='flex flex-col items-center '>
-                <Image 
-                    src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}  
+                <Image
+                    src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
                     alt='icon'
                     width='100'
                     height='100'
@@ -29,7 +29,7 @@ const Weather = ({weather}) => {
                     <p className='text-lg'>Humidity</p>
                 </div>
                 <div>
-                    <p className='font-bold text-2xl'>{(weather.wind.speed.toFixed(1)* 1.60934).toFixed(2)} KM/H</p>
+                    <p className='font-bold text-2xl'>{(weather.wind.speed.toFixed(1) * 1.60934).toFixed(2)} KM/H</p>
                     <p className='text-lg'>Winds</p>
                 </div>
             </div>
